367a0bac281514140163e53cc47abc00b1ea4126
YustR2hev <46708502+YustR2hev@users.noreply.github.com>
2021-04-26 17:52:03 +1000

    Update reaction_equation.py

--- reaction_equation.py
+++ reaction_equation.py
@@ -10,34 +10,34 @@ def parse_compound(compound):
 
 def main(data):
     in_ = data
-    lhs_strings = in_.replace(' ', '').split('=')[0].split('+')
-    lhs_compounds = [parse_compound(compound) for compound in lhs_strings]
+    left_half_strings = in_.replace(' ', '').split('=')[0].split('+')
+    left_half_compounds = [parse_compound(compound) for compound in left_half_strings]
 
-    rhs_strings = in_.replace(' ', '').split('=')[1].split('+')
-    rhs_compounds = [parse_compound(compound) for compound in rhs_strings]
+    right_half_strings = in_.replace(' ', '').split('=')[1].split('+')
+    right_half_compounds = [parse_compound(compound) for compound in right_half_strings]
 
-    els = sorted(set().union(*lhs_compounds, *rhs_compounds))
+    els = sorted(set().union(*left_half_compounds, *right_half_compounds))
     els_index = dict(zip(els, range(len(els))))
 
-    w = len(lhs_compounds) + len(rhs_compounds)
+    w = len(left_half_compounds) + len(right_half_compounds)
     h = len(els)
-    A = [[0] * w for _ in range(h)]
-    for col, compound in enumerate(lhs_compounds):
+    pre_res = [[0] * w for _ in range(h)]
+    for col, compound in enumerate(left_half_compounds):
         for el, num in compound.items():
             row = els_index[el]
-            A[row][col] = num
-    for col, compound in enumerate(rhs_compounds, len(lhs_compounds)):
+            pre_res[row][col] = num
+    for col, compound in enumerate(right_half_compounds, len(left_half_compounds)):
         for el, num in compound.items():
             row = els_index[el]
-            A[row][col] = -num
+            pre_res[row][col] = -num
 
-    A = sympy.Matrix(A)
-    coeffs = A.nullspace()[0]
-    coeffs *= sympy.lcm([term.q for term in coeffs])
+    pre_res = sympy.Matrix(pre_res)
+    ratio = pre_res.nullspace()[0]
+    ratio *= sympy.lcm([term.q for term in ratio])
 
-    lhs = " + ".join(["{} {}".format(coeffs[i], s) for i, s in enumerate(lhs_strings)])
-    rhs = " + ".join(["{} {}".format(coeffs[i], s) for i, s in enumerate(rhs_strings, len(lhs_strings))])
-    return f"{lhs} -> {rhs}"
+    left_half = " + ".join(["{} {}".format(ratio[i], s) for i, s in enumerate(left_half_strings)])
+    right_half = " + ".join(["{} {}".format(ratio[i], s) for i, s in enumerate(right_half_strings, len(left_half_strings))])
+    return f"{left_half} -> {right_half}"
 
 
 if __name__ == "__main__":
